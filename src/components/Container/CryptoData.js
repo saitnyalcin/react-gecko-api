2fcddd4cfe58f7948c4ef8419a082f70e91bd928
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-09-23 12:38:12 -0400

    Loading state is implemented

--- src/components/Container/CryptoData.js
+++ src/components/Container/CryptoData.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import AuthenticationAPI from "../../services/coinGeckoService";
+import AuthenticationAPI from '../../services/coinGeckoService'
+import bitcoinLoader from '../../assets/bitcoinLoading.gif'
+
 
 const CryptoData = () => {
   const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const fetchData = async () => {
     await fetch(AuthenticationAPI.API_URL())
       .then((response) => response.json())
-      .then((data) => setData(data))
+      .then((data) => {
+        if (data) {
+          setData(data)
+          setLoading(false)
+        }
+      })
       .then((result) => {
         console.log('Success:', result)
       })
@@ -17,27 +25,37 @@ const CryptoData = () => {
   }
 
   useEffect(() => {
-    fetchData()
+    setTimeout(() => {
+      fetchData()
+    }, 3000)
   }, [])
 
   return (
     <div>
+      {loading && (
+        <div>
+          <img
+            src={bitcoinLoader}
+            alt="crypto_image"
+            width="100"
+            height="100"
+          />
+        </div>
+      )}
       <div className="crypto-container">
-        {data
-          .slice(0, 10)
-          .map((crypto) => (
-            <div key={crypto.id}>
-              <div>
-                <div className="crypto-title">{crypto.name}</div>
-                <img
-                  src={crypto.image.large}
-                  alt="crypto_image"
-                  width="100"
-                  height="100"
-                />
-              </div>
+        {data.slice(0, 10).map((crypto) => (
+          <div key={crypto.id}>
+            <div>
+              <div className="crypto-title">{crypto.name}</div>
+              <img
+                src={crypto.image.large}
+                alt="crypto_image"
+                width="100"
+                height="100"
+              />
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   )
